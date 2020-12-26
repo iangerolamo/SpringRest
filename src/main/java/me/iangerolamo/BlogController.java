e91bd4c99403152d7af2eade34d91e1ac7fd6673
Ian Gerolamo <iangerolamo@gmail.com>
2020-12-26 13:43:14 -0300

    Building a Spring Boot REST API

---
+++ src/main/java/me/iangerolamo/BlogController.java
@@ -0,0 +1,13 @@
+package me.iangerolamo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Congratulations from BlogController.java";
+    }
+}
